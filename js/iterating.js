2bfe084fff3ac0dd4b113517a2e8f25f24d64bc5
mario welch <mario.welch01@gmail.com>
2021-10-19 13:50:36 -0500

    completed iterating arrays exercise

---
+++ js/iterating.js
@@ -0,0 +1,72 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+    var names = ['Mario', 'Keith', 'Alexis', 'Maverick'];
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+    console.log(names.length)
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+    for(var n = 0; n < names.length; n++) {
+        console.log(names[n]);
+    }
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+    names.forEach(function(name, index, array){
+        console.log('Name #' + (index + 1) + " of " + array.length + ' is: ' + name);
+    });
+
+    names.forEach(function(name){
+        console.log('One of the names is ' + name);
+    });
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+    function first(arr) {
+        return arr[0];
+    }
+    function second(arr) {
+        return arr[1];
+    }
+
+    function last(arr) {
+        var lastItem = arr.length - 1
+        return arr[lastItem];
+    }
+
+    console.log(first([1, 2, 3]));
+    console.log(second([4, 5, 6]));
+    console.log(last([7, 8, 9, 10]));
+})();
